446fd41fe819ac2a3e541a1b86dfbd8f52fabbb9
Filipos <filpap9@gmail.com>
2025-10-20 21:07:56 +0300

    Switch to Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs20.x',
+			// If you're using edge functions, add:
+			// edge: false,
+			// If your functions need more memory/time:
+			// maxDuration: 10
+		}),
 		experimental: {
 			remoteFunctions: true
 		}
