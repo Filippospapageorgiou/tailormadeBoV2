8b5146df5b824dde5c3469df8f278cc7b326b156
Filipos <filpap9@gmail.com>
2025-11-14 14:24:54 +0200

    user_acceoptense bug

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -1,36 +1,37 @@
 import type { PageServerLoad } from './$types';
 import type { Actions } from './$types';
 
-
-export const load:PageServerLoad = async({ locals: { supabase, user }}) => {
-
-    const { data: blog , error: BlogError } = await supabase
-        .from('blogs')
-        .select(`
+export const load: PageServerLoad = async ({ locals: { supabase, user } }) => {
+	const { data: blog, error: BlogError } = await supabase
+		.from('blogs')
+		.select(
+			`
             *,
             profile:profiles!blogs_author_id_fkey (
                 username,
                 image_url
-            )`)
-        .eq('published', true)
-        .order('created_at', { ascending: false })
-        .limit(1)
-        .single();
-    
-    if(BlogError){
-        console.error('Error fetching blog:', BlogError);
-        return {
-            blog:null,
-            tos:null,
-            error: BlogError.message
-        };
-    }
+            )`
+		)
+		.eq('published', true)
+		.order('created_at', { ascending: false })
+		.limit(1)
+		.single();
 
-    // --- Check if user has accepted ToS ---
+	if (BlogError) {
+		console.error('Error fetching blog:', BlogError);
+		return {
+			blog: null,
+			tos: null,
+			error: BlogError.message
+		};
+	}
+
+	// --- Check if user has accepted ToS ---
 	const { data: tos, error: tosError } = await supabase
 		.from('user_term_acceptance')
 		.select('*')
 		.eq('user_id', user?.id)
+		.eq('is_accepted', true)
 		.maybeSingle();
 
 	if (tosError) {
@@ -43,15 +44,12 @@ export const load:PageServerLoad = async({ locals: { supabase, user }}) => {
 	}
 
 	const tosFlag = user?.id ? !!tos : false;
-    
 
-    return {
-        blog: blog || null,
-        tos:tosFlag
-    }
-}
-
-
+	return {
+		blog: blog || null,
+		tos: tosFlag
+	};
+};
 
 export const actions = {
 	acceptTerms: async ({ request, locals: { supabase, user } }) => {
@@ -65,13 +63,11 @@ export const actions = {
 			};
 		}
 
-		const { error } = await supabase
-			.from('user_term_acceptance')
-			.insert({
-				user_id: user.id,
-				is_accepted: true,
-				created_at: new Date().toISOString()
-			});
+		const { error } = await supabase.from('user_term_acceptance').insert({
+			user_id: user.id,
+			is_accepted: true,
+			created_at: new Date().toISOString()
+		});
 
 		if (error) {
 			console.error('Error saving ToS acceptance:', error);
